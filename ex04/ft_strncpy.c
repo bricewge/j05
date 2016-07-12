349495f05e3fcc0c6ab32f73396c53566a21b4d6
Brice WAEGENEIRE <bricewge@gmail.com>
2016-07-12 16:11:05 +0200

    fix ex04

--- ex04/ft_strncpy.c
+++ ex04/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: bwaegene <brice.wge@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/11 23:12:43 by bwaegene          #+#    #+#             */
-/*   Updated: 2016/07/12 09:53:32 by bwaegene         ###   ########.fr       */
+/*   Updated: 2016/07/12 16:10:54 by bwaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	while (i < n)
 	{
 		dest[i] = src[i];
+		i++;
+	}
+	if (src[i])
+	{
+		dest[i] = '\0';
 		i++;
 	}
 	dest[i] = '\0';
