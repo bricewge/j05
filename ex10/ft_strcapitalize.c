047405ac58d3866e31e1e03e4e54797b1d5fc712
Brice WAEGENEIRE <bricewge@gmail.com>
2016-07-12 17:17:21 +0200

    ex10

--- ex10/ft_strcapitalize.c
+++ ex10/ft_strcapitalize.c
@@ -6,28 +6,47 @@
 /*   By: bwaegene <brice.wge@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/12 16:44:47 by bwaegene          #+#    #+#             */
-/*   Updated: 2016/07/12 17:11:49 by bwaegene         ###   ########.fr       */
+/*   Updated: 2016/07/12 19:08:10 by bwaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+char	ft_upcase(char str)
+{
+	if (str >= 'a' && str <= 'z')
+		str = str - ' ';
+	return (str);
+}
+
+char	ft_lowcase(char str)
+{
+	if (str >= 'A' && str <= 'Z')
+		str = str + ' ';
+	return (str);
+}
 
 char	*ft_strcapitalize(char *str)
 {
 	int		i;
-	int		alphanum;
+	int		fst_letter;
 
 	i = 0;
-	alphanum = 1;
+	fst_letter = 1;
 	while (str[i])
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if ((str[i] >= 'a' && str[i] <= 'z')
+			|| (str[i] >= 'A' && str[i] <= 'Z')
+			|| (str[i] >= '0' && str[i] <= '9'))
 		{
-			str[i] = str[i] - alphanum * ' ';
-			alphanum = 0;
+			if (fst_letter == 1)
+			{
+				str[i] = ft_upcase(str[i]);
+				fst_letter = 0;
+			}
+			else
+				str[i] = ft_lowcase(str[i]);
 		}
-		else if (str[i] >= '0' && str[i] <= '9')
-			alphanum = 0;
 		else
-			alphanum = 1;
+			fst_letter = 1;
 		i++;
 	}
 	return (str);
